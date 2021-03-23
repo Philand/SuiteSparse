c11ee0744a98dbc28867767a36dc2220ca5efb18
Philand <philand.pincon@free.fr>
2021-03-23 14:08:41 +0100

    Update of the analysis in is_demo.c

--- CSparse/Demo/is_demo.c
+++ CSparse/Demo/is_demo.c
@@ -156,29 +156,44 @@ int main (void)
                 " Beginning of the main for loop  \n" \
                 "================================ \n") ;
 
-        printf("column number : n = %td\n", n) ;
+        printf("column number : n = %td\n", n, \
+               "------------------------------\n") ;
 
         for (k = 0 ; k < n ; k++)       /* compute L(k,:) for L*L' = C */
         {
-            printf ("k = %td\n", k) ;
+            printf ("-------------------------------------------------------" \
+                    "k = %td\n", k) ;
 
+            printf ("--- Nonzero pattern of L(%td,:) --- \n", k) ;
             /* --- Nonzero pattern of L(k,:) ------------------------------------ */
             top = cs_ereach (C, k, parent, s, c) ;      /* find pattern of L(k,:) */
             printf ("top = %td\n", top) ;
             x [k] = 0 ;                                 /* x (0:k) is now zero */
+            printf ("Start of the intern loop for (p) from Cp[%td] = %td to Cp[%td] = %td \n", k, Cp [k], k, Cp [k+1]) ;
             for (p = Cp [k] ; p < Cp [k+1] ; p++)       /* x = full(triu(C(:,k))) */
             {
-                if (Ci [p] <= k) 
+                
+                if (Ci [p] <= k)
+                {
                     x [Ci [p]] = Cx [p] ;
+                    printf ("x [Ci [p]] = Cx [p] = %f \n", x [Ci [p]]) ;
+                }
             }
             d = x [k] ;                     /* d = C(k,k) */
+            printf ("d = %f\n", d, "d = C(k,k)") ;
             x [k] = 0 ;                     /* clear x for k+1st iteration */
             /* --- Triangular solve --------------------------------------------- */
+            printf ("--- Triangular solve --- \n") ;
+            printf ("Start of the intern loop for (top) from %td to n = %td \n", top, n) ;
             for ( ; top < n ; top++)    /* solve L(0:k-1,0:k-1) * x = C(:,k) */
             {
                 i = s [top] ;               /* s [top..n-1] is pattern of L(k,:) */
+                printf ("i = %td \n", i) ;
                 lki = x [i] / Lx [Lp [i]] ; /* L(k,i) = x (i) / L(i,i) */
+                printf ("lki = %f \n", lki) ;
                 x [i] = 0 ;                 /* clear x for k+1st iteration */
+
+                printf ("Start of the intern loop for from Lp[%td] = %td to c[%td] = %td \n", i, Lp [i] + 1, i, c [i]) ;
                 for (p = Lp [i] + 1 ; p < c [i] ; p++)
                 {
                     x [Li [p]] -= Lx [p] * lki ;
